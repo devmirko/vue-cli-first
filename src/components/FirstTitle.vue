69a9dcffb1c3e4f48cd99505de1a39f35f17f47f
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-06-07 17:07:56 +0200

    fine esercizio

---
+++ src/components/FirstTitle.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="hello">
+       <h2>La mia prima app con Vue CLI</h2>
+    </div>
+
+</template>
+
+<script>
+export default {
+  name: 'FirstTitle',
+
+}
+</script>
+
+<style scoped lang="scss">
+h2 {
+  margin: 40px 0 0;
+  color: black;
+}
+
+</style>
